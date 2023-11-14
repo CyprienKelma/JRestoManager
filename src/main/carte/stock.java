ad3bb76ef33f60b4bfa2f9a4f8a7d351df659096
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-11-14 18:47:43 +0100

    c

---
+++ src/main/carte/stock.java
@@ -0,0 +1,23 @@
+package main.carte;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import main.carte.aliment;
+
+public class stock {
+
+    public static String LireFichier(String fichier) throws IOException{
+        
+    }
+
+
+    public static String ModifierFichier(String fichier) throws IOException{
+        
+    }
+
+
+    public static void main(String[] args) {
+       
+        
+    }
+}
