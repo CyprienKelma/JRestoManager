2ea3a965af60734afc0ea74cc716453cf26094ce
Cyprien Kelma <cyprien.kelma@gmail.com>
2023-11-14 17:22:15 +0100

    Add the Restaurant class base code

--- src/main/place/Restaurant.java
+++ src/main/place/Restaurant.java
@@ -8,5 +8,13 @@ public class Restaurant {
     private boolean isOpened;
     private List<Employé> employésList;
     private List<Equipe> équipesList;
+    // TODO add a stock fields (from the "Stock" class)
+
+    private List<Transaction> listTransactions = null;
+
+    // Need the implementation and import of "Carte" (a Class) and "Carteboissons"
+    // (an enum)
+    private Carte menu;
+    private CarteBoissons menuBoissons;
 
 }
---
+++ src/main/place/Transaction.java
@@ -0,0 +1,8 @@
+package main.place;
+
+import java.util.List;
+import main.staff.*;
+
+public class Transaction {
+
+}
