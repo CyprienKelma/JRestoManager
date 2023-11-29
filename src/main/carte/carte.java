0e7fa9d69ae6366b785c26608de5db3ba408a35f
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-11-29 14:32:33 +0100

    c

--- src/main/carte/carte.java
+++ src/main/carte/carte.java
@@ -4,68 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class carte {
-    List <String> Liste1 = new ArrayList<>(){
-            {
-                add("Salade Tomates");
-                add("Salade simple");
-                add("Potage Oignon");
-                add("Potage Tomates");
-                add("Potage champignon");
-                add("Burger");
-                add("Burger Sans T");
-                add("Burger Sans TS");
-                add("Pizza formage");
-                add("Pizza champignon");
-                add("Pizza Chorizo");
-            }
 
+    List<String> liste1 = new ArrayList<>() {
+        {
+            add("Salade Tomates");
+            add("Salade simple");
+            add("Potage Oignon");
+            add("Potage Tomates");
+            add("Potage champignon");
+            add("Burger");
+            add("Burger Sans T");
+            add("Burger Sans TS");
+            add("Pizza fromage");
+            add("Pizza champignon");
+            add("Pizza Chorizo");
+        }
     };
 
-    List <String> Liste2 = new ArrayList<>(){
-            {
-                add("9€");
-                add("9€");
-                add("8€");
-                add("8€");
-                add("8€");
-                add("15€");
-                add("15€");
-                add("15€");
-                add("12€");
-                add("12€");
-                add("12€");              
-            }
-
+    List<String> liste2 = new ArrayList<>() {
+        {
+            add("9€");
+            add("9€");
+            add("8€");
+            add("8€");
+            add("8€");
+            add("15€");
+            add("15€");
+            add("15€");
+            add("12€");
+            add("12€");
+            add("12€");
+        }
     };
 
-    List <String> Liste3 = new ArrayList<>(){
-            {
-                add("Salade Tomate");
-                add("Salade");
-                add("3*Oignon");
-                add("3*Tomate");
-                add("3*champignon");
-                add("Pain Salade Tomate Viande");
-                add("Pain Salade Viande");
-                add("Pain Viande");
-                add("Pain Tomate Fromage");
-                add("Pain Tomate Fromage Champignon");
-                add("Pain Tomate Fromage Chorizo");
-            }
-
+    List<String> liste3 = new ArrayList<>() {
+        {
+            add("Salade Tomate");
+            add("Salade");
+            add("3*Oignon");
+            add("3*Tomate");
+            add("3*champignon");
+            add("Pain Salade Tomate Viande");
+            add("Pain Salade Viande");
+            add("Pain Viande");
+            add("Pain Tomate Fromage");
+            add("Pain Tomate Fromage Champignon");
+            add("Pain Tomate Fromage Chorizo");
+        }
     };
 
-    List <String> Liste4 = new ArrayList<>(){
+    List<String> liste4 = new ArrayList<>() {
         {
             add("Limonade");
             add("Cidre doux");
-            add("Biere sans alcol");
-            add("Jus de Fruis");
+            add("Biere sans alcool");
+            add("Jus de Fruits");
             add("Verre d'eau");
         }
     };
 
-    List <String> Liste5 = new ArrayList<>(){
+    List<String> liste5 = new ArrayList<>() {
         {
             add("4");
             add("5");
@@ -74,24 +72,31 @@ public class carte {
             add("0");
         }
     };
-    
-    List <String> Plat = new ArrayList<>();
-    Plat.addAll(liste1);
-    Plat.addAll(liste2);
-    Plat.addAll(liste3);
 
-
-    public void VerifieDispoPlat(List Plats, List Stock){
-
+    List<String> plat = new ArrayList<>();
+    public void initialiserPlat() {
+        plat.addAll(liste1);
+        plat.addAll(liste2);
+        plat.addAll(liste3);
     }
 
-    public static String AfficherMenu(List Plats, List Boissons){
-        systeme.out.println("La Liste des plats : \n");
-        systeme.out.println(Plats);
-        systeme.out.println("\n\n")
-
-        systeme.out.println("La Liste des Boissons : \n");
-        syteme.out.println(Boissons)
+    List<String> boisson = new ArrayList<>();
+    public void initialiserBoisson() {
+        plat.addAll(liste4);
+        plat.addAll(liste5);
     }
 
+
+    public void verifieDispoPlat(List<String> plats, List<String> stock) {
+        // Ajoutez ici la logique de vérification de la disponibilité des plats par rapport au stock.
+    }
+
+    public static void afficherMenu(List<String> plats, List<String> boissons) {
+        System.out.println("La Liste des plats : \n");
+        System.out.println(plats);
+        System.out.println("\n\n");
+
+        System.out.println("La Liste des Boissons : \n");
+        System.out.println(boissons);
+    }
 }
--- src/main/carte/stock.java
+++ src/main/carte/stock.java
@@ -1,42 +1,77 @@
 package main.carte;
-import java.io.IOException;
-import java.io.BufferedWriter;
+
 import java.io.BufferedReader;
-import main.carte.aliment;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class stock {
 
-    public static String LireFichier(String nomfichier) throws IOException{
-        Path fichier = Path.of(nomfichier);
-        Charset charset = charset.forName("windows-1252");
-        if(Files.exists(fichier) == true){
-            boolean estAccessible = Files.isRegularFile(fichier) 
-            && Files.isReadable(fichier) && Files.isExecutable(fichier);
-            if(estAccessible == true){
-                
+    public static List<aliment> lireFichier(String nomFichier) throws IOException {
+        Path fichier = Path.of(nomFichier);
+        Charset charset = Charset.forName("windows-1252");
+        List<aliment> listeAliments = new ArrayList<>();
 
-
+        if (Files.exists(fichier)) {
+            boolean estAccessible = Files.isRegularFile(fichier)
+                    && Files.isReadable(fichier) && Files.isExecutable(fichier);
+            
+            if (estAccessible) {
+                try (BufferedReader reader = Files.newBufferedReader(fichier, charset)) {
+                    String ligne;
+                    while ((ligne = reader.readLine()) != null) {
+                        // Supposons que chaque ligne du fichier a le format "nom,quantite"
+                        String[] elements = ligne.split(",");
+                        if (elements.length == 2) {
+                            String nom = elements[0].trim();
+                            int quantite = Integer.parseInt(elements[1].trim());
+                            aliment aliment = new aliment(nom, quantite);
+                            listeAliments.add(aliment);
+                        }
+                    }
+                }
             }
+        }
+
+        return listeAliments;
+    }
 
 
-
+    public static void ajouterAliment(String nomFichier, aliment aliment) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(nomFichier), Charset.forName("windows-1252"))) {
+            // Écrivez les détails de l'aliment dans le fichier
+            // Format : nom,quantite
+            writer.write(aliment.getNom() + "," + aliment.getQuantite());
         }
     }
 
-    public static String AjouterAliment(String nomfichier) throws IOException{
-        
-    }
+    public static void retirerAliment(String nomFichier, String nomAliment) throws IOException {
+        List<aliment> listeAliments = lireFichier(nomFichier);
 
-    public static String RetirerAliment(String nomfichier) throws IOException{
-        
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(nomFichier), Charset.forName("windows-1252"))) {
+            for (aliment aliment : listeAliments) {
+                if (!aliment.getNom().equals(nomAliment)) {
+                    // Réécrire l'aliment dans le fichier sauf celui correspondant à l'aliment à retirer
+                    writer.write(aliment.getNom() + "," + aliment.getQuantite());
+                    writer.newLine();
+                }
+            }
+        }
     }
-
 
     public static void main(String[] args) {
-       
-        
+        // Exemple d'utilisation
+        try {
+            List<aliment> aliments = lireFichier("votre_fichier.txt");
+            for (aliment aliment : aliments) {
+                System.out.println(aliment.getNom() + " - " + aliment.getQuantite());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
