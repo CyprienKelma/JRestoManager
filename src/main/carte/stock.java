0eabc317e9c47044a50a970c217186769488f639
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-11-30 18:45:22 +0100

    masterclass

--- src/main/carte/stock.java
+++ src/main/carte/stock.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 
 public class stock {
 
@@ -68,39 +70,29 @@ public class stock {
     }
     
 
-    public static void retirerAliment(String nomFichier, aliment aliment) throws IOException {
+    public static void retirerAliment(String nomFichier, Map<String, Integer> ingredients) throws IOException {
         List<aliment> listeAliments = lireFichier(nomFichier);
     
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(nomFichier), Charset.forName("windows-1252"))) {
             for (aliment existingAliment : listeAliments) {
-                if (existingAliment.getNom().equals(aliment.getNom())) {
-                    int nouvelleQuantite;
-                    // Additionner la quantité existante avec la quantité de l'aliment à ajouter
-                    if(existingAliment.getQuantite()>aliment.getQuantite()){
-                        nouvelleQuantite = existingAliment.getQuantite() - aliment.getQuantite();
-                    }
-                    else nouvelleQuantite = 0;
-                    // Écrire l'aliment mis à jour avec la nouvelle quantité dans le fichier
+                // Vérifier si l'aliment actuel est dans la liste d'ingrédients spécifiée
+                if (ingredients.containsKey(existingAliment.getNom())) {
+                    int quantiteARetirer = ingredients.get(existingAliment.getNom());
+    
+                    // Mise à jour de la quantité en vérifiant qu'elle ne devient pas négative
+                    int nouvelleQuantite = Math.max(existingAliment.getQuantite() - quantiteARetirer, 0);
+    
+                    // Écrire dans le fichier le nouvel aliment avec la quantité mise à jour
                     writer.write(existingAliment.getNom() + "," + nouvelleQuantite);
                     writer.newLine();
                 } else {
-                    // Réécrire les autres aliments tels quels dans le fichier
+                    // Si ce n'est pas un ingrédient à retirer, réécrire dans le fichier
                     writer.write(existingAliment.getNom() + "," + existingAliment.getQuantite());
                     writer.newLine();
                 }
             }
-    
-            // Si l'aliment à ajouter n'était pas présent dans la liste existante, l'ajouter à la fin du fichier
-            if (!listeAliments.stream().anyMatch(a -> a.getNom().equals(aliment.getNom()))) {
-                writer.write(aliment.getNom() + "," + 0);
-                writer.newLine();
-            }
         }
     }
-
-    
-    
-    
     
     
     
@@ -108,18 +100,26 @@ public class stock {
 
     /*public static void main(String[] args) {
         // Exemple d'utilisation
-        try {
-            aliment tomate = new aliment("tomate", 4);
-            ajouterAliment("src\\main\\data\\stock.txt", tomate);
-            aliment salade = new aliment("salade", 1);
-            retirerAliment("src\\main\\data\\stock.txt", salade);
+        carte carteInstance = new carte();
 
-            List<aliment> aliments = lireFichier("src\\main\\data\\stock.txt");
-            for (aliment aliment : aliments) {
-                System.out.println(aliment.getNom() + " - " + aliment.getQuantite());
-            }
+        // Initialisation des plats et boissons
+        carteInstance.initialiserPlat();
+        carteInstance.initialiserBoisson();
+
+        // Exemple de plat à retirer
+        String platARetirer = "Potage tomate";
+
+        // Récupération des ingrédients du plat à retirer
+        Map<String, Integer> ingredientsAretirer = carteInstance.getIngredients(platARetirer);
+
+        System.out.println(ingredientsAretirer);
+        try {
+            // Appel de la fonction pour retirer l'aliment du stock
+            stock.retirerAliment("src\\main\\data\\stock.txt", ingredientsAretirer);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }*/
 }
+
