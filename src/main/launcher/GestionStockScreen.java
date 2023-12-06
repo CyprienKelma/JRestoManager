53c22f00659d23fd39342037de48f5f4cc55d14d
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-12-06 12:13:54 +0100

    c

--- src/main/launcher/GestionStockScreen.java
+++ src/main/launcher/GestionStockScreen.java
@@ -1,12 +1,16 @@
 package main.launcher;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 import main.carte.Aliment;
+import main.carte.Boisson;
 import main.carte.Stock;
+import main.carte.StockDrink;
 import main.place.Restaurant;
 
 //Classe qui gère l'écran d
@@ -39,6 +43,7 @@ public class GestionStockScreen {
             } else {
                 tryShowingGestionScreen(menuScanner);
             }
+            
         }
         showGestionScreen(menuScanner);
     }
@@ -67,12 +72,13 @@ public class GestionStockScreen {
                 break;
             case "2":
                 // Appelle fontion pour gestion stock Aliment Boissson
-                //showGestionBoissonScreen(menuScanner);
+                showGestionBoissonScreen(menuScanner);
                 break;
             default:
                 tryShowingGestionScreen(menuScanner);
         }
     }
+//----------------------------------------PLATS----------------------------------------------------------
 
     public static void showGestionPlatScreen(Scanner menuScanner) throws IOException {
         clearConsole();
@@ -193,7 +199,14 @@ public class GestionStockScreen {
                 } while (!alimentExiste);
 
                 print("\nEntrez la quantité de l'aliment : \n");
-                int quantiteAliment = menuScanner.nextInt();
+                int quantiteAliment;
+                do {
+                    // Assurez-vous que la quantité est positive
+                    quantiteAliment = menuScanner.nextInt();
+                    if (quantiteAliment <= 0) {
+                        print("La quantité doit être positive. Veuillez réessayer.\n");
+                    }
+                } while (quantiteAliment <= 0);
 
                 // Créer une instance d'Aliment avec les informations fournies
                 Aliment nouvelAliment = new Aliment(nomAliment, quantiteAliment);
@@ -217,7 +230,7 @@ public class GestionStockScreen {
         print("--------------------------------------------------------------------------\n");
         print("0 - Retour au menu gestion de stock des aliments des plats\n");
         print("1 - Retirer des aliments au stock des aliments des plats\n");
-
+    
         String choixEcran = menuScanner.next();
         switch (choixEcran) {
             case "0":
@@ -236,13 +249,167 @@ public class GestionStockScreen {
                 boolean alimentExiste = false;
                 String nomAliment;
                 do {
+                    // Nettoyer le tampon avant de lire le nom de l'aliment
+                    menuScanner.nextLine();
+    
                     // Demander à l'utilisateur le nom de l'aliment à retirer
                     print("Entrez le nom de l'aliment à retirer : \n");
-                    nomAliment = menuScanner.next();
-
-                    // Vérifier si l'aliment est dans la liste
+                    nomAliment = menuScanner.nextLine();
+    
+                    // Vérifier si l'aliment est dans la liste (en comparant le nom complet)
                     for (Aliment aliment : listeAliments) {
-                        if (aliment.getNom().equals(nomAliment)) {
+                        // Utilisez equalsIgnoreCase pour la comparaison insensible à la casse
+                        if (aliment.getNom().equalsIgnoreCase(nomAliment)) {
+                            alimentExiste = true;
+                            break;
+                        }
+                    }
+    
+                    if (!alimentExiste) {
+                        print("Cet aliment n'est pas dans la liste. Veuillez réessayer.\n");
+                    }
+    
+                } while (!alimentExiste);
+    
+                // Demander à l'utilisateur la quantité de l'aliment à retirer
+                print("\nEntrez la quantité de l'aliment à retirer : \n");
+                int quantiteAliment;
+                do {
+                    // Assurez-vous que la quantité est positive et ne dépasse pas la quantité en stock
+                    quantiteAliment = menuScanner.nextInt();
+                    int quantiteEnStock = getQuantiteEnStock(listeAliments, nomAliment);
+                    if (quantiteAliment <= 0 || quantiteAliment > quantiteEnStock) {
+                        print("La quantité doit être positive et ne pas dépasser la quantité en stock. Veuillez réessayer.\n");
+                    }
+                } while (quantiteAliment <= 0);
+    
+                // Créer une Map avec le nom de l'aliment et la quantité à retirer
+                Map<String, Integer> ingredientsAretirer = Map.of(nomAliment, quantiteAliment);
+    
+                // Appel de la fonction pour retirer l'aliment du stock
+                Stock.retirerAliment("src\\main\\data\\stock.txt", ingredientsAretirer);
+    
+                RetirerAlimentPlatScreen(menuScanner);
+                break;
+            default:
+                showGestionPlatScreen(menuScanner);
+        }
+    }
+//--------------------------------------------------------------------------------------------------------
+
+//----------------------------------------Boissson--------------------------------------------------------
+
+    public static void showGestionBoissonScreen(Scanner menuScanner) throws IOException {
+        clearConsole();
+        print("==========================================================================\n");
+        print("Menu de Gestion du Stock des aliments pour les boisons : \n");
+        print("--------------------------------------------------------------------------");
+        print("                   Que souhaitez-vous faire ?");
+        print("--------------------------------------------------------------------------\n");
+        print("0 - Retour au menu gestion de stock\n");
+        print("1 - Afficher le Stock des aliments pour les boissons\n");
+        print("2 - Ajouter des aliments pour les boissons\n");
+        print("3 - Retirer des aliments pour les boissonS\n");
+
+        String choixEcran = menuScanner.next();
+
+        switch (choixEcran) {
+            case "0":
+                // Retour du screen
+                showGestionScreen(menuScanner);
+                break;
+            case "1":
+                // Appelle fontion pour afficher stock Aliment Boisson
+                showAlimentBoissonScreen(menuScanner);
+                break;
+            case "2":
+                // Appelle fontion pour ajouter Aliment Boisson
+                AjouterAlimentBoissonScreen(menuScanner);
+                break;
+            case "3":
+                // Appelle fontion pour retirer Aliment Boisson
+                RetirerAlimentBoissonScreen(menuScanner);
+                break;
+            default:
+                showGestionScreen(menuScanner);
+        }
+    }
+
+    public static void showAlimentBoissonScreen(Scanner menuScanner) throws IOException {
+        clearConsole();
+        print("==========================================================================\n");
+        print("Menu de Gestion du Stock des aliments pour les boissons : \n");
+        print("--------------------------------------------------------------------------");
+        print("                  Affichage des aliments des boissons");
+        print("--------------------------------------------------------------------------\n");
+ 
+        // Utilisation de la fonction lireFichier de la classe Stock pour récupérer la liste d'aliments
+        List<Boisson> listeAliments = StockDrink.lireFichier("src\\main\\data\\stockDrink.txt");
+
+        // Affichage des aliments
+        for (Boisson aliment : listeAliments) {
+            System.out.println(aliment.getNom() + ": " + aliment.getQuantite());
+        }
+
+        print("\n--------------------------------------------------------------------------\n");
+        print("0 - Retour au menu gestion de stock des aliments des boissons\n");
+        print("1 - Réafficher le stock des aliments des boissons\n");
+        String choixEcran = menuScanner.next();
+
+        switch (choixEcran) {
+            case "0":
+                // Retour du screen
+                showGestionBoissonScreen(menuScanner);
+                break;
+            case "1":
+                // Rétour a la fonction
+                showAlimentBoissonScreen(menuScanner);
+                break;
+            default:
+                showGestionBoissonScreen(menuScanner);
+        }
+    }
+
+    public static void AjouterAlimentBoissonScreen(Scanner menuScanner) throws IOException {
+        clearConsole();
+        print("==========================================================================\n");
+        print("Menu de Gestion du Stock des aliments pour les boissons : \n");
+        print("--------------------------------------------------------------------------");
+        print("                  Ajouter des aliments des boissons");
+        print("--------------------------------------------------------------------------\n");
+        print("0 - Retour au menu gestion de stock des aliments des boissons\n");
+        print("1 - Ajouter des aliments au stock des aliments des boissons\n");
+    
+        String choixEcran = menuScanner.next();
+        switch (choixEcran) {
+            case "0":
+                // Retour du screen
+                showGestionBoissonScreen(menuScanner);
+                break;
+            case "1":
+    
+                // Utilisation de la fonction lireFichier de la classe Stock pour récupérer la liste d'aliments
+                List<Boisson> listeAliments = StockDrink.lireFichier("src\\main\\data\\stockDrink.txt");
+                print("\n--------------------------------------------------------------------------\n");
+                // Affichage des aliments
+                for (Boisson aliment : listeAliments) {
+                    System.out.println(aliment.getNom() + ": " + aliment.getQuantite());
+                }
+                print("\n--------------------------------------------------------------------------\n");
+                boolean alimentExiste = false;
+                String nomAliment;
+                do {
+                    // Nettoyer le tampon avant de lire le nom de l'aliment
+                    menuScanner.nextLine();
+
+                    // Demander à l'utilisateur le nom de l'aliment à ajouter
+                    print("Entrez le nom de l'aliment à ajouter : \n");
+                    nomAliment = menuScanner.nextLine();
+
+                    // Vérifier si l'aliment est dans la liste (en comparant le nom complet)
+                    for (Boisson aliment : listeAliments) {
+                        // Utilisez equalsIgnoreCase pour la comparaison insensible à la casse
+                        if (aliment.getNom().equalsIgnoreCase(nomAliment)) {
                             alimentExiste = true;
                             break;
                         }
@@ -253,24 +420,124 @@ public class GestionStockScreen {
                     }
 
                 } while (!alimentExiste);
-
-                // Demander à l'utilisateur la quantité de l'aliment à retirer
-                print("\nEntrez la quantité de l'aliment à retirer : \n");
-                int quantiteARetirer = menuScanner.nextInt();
-
-                // Créer une Map avec le nom de l'aliment et la quantité à retirer
-                Map<String, Integer> ingredientsAretirer = Map.of(nomAliment, quantiteARetirer);
-
-                // Appel de la fonction pour retirer l'aliment du stock
-                Stock.retirerAliment("src\\main\\data\\stock.txt", ingredientsAretirer);
-
-                RetirerAlimentPlatScreen(menuScanner);
+    
+                print("\nEntrez la quantité de l'aliment : \n");
+                int quantiteAliment;
+                do {
+                    // Assurez-vous que la quantité est un entier positif
+                    try {
+                        quantiteAliment = menuScanner.nextInt();
+                        if (quantiteAliment <= 0) {
+                            print("La quantité doit être positive. Veuillez réessayer.\n");
+                        }
+                    } catch (InputMismatchException e) {
+                        print("Veuillez entrer un nombre entier. Veuillez réessayer.\n");
+                        quantiteAliment = -1; // Définir une valeur par défaut pour éviter une boucle infinie
+                    }
+                } while (quantiteAliment <= 0);
+    
+                // Créer une instance de Boisson avec les informations fournies
+                Boisson nouvelAliment = new Boisson(nomAliment, quantiteAliment);
+    
+                // Appel de la fonction pour ajouter l'aliment au stock
+                StockDrink.ajouterBoisson("src\\main\\data\\stockDrink.txt", nouvelAliment);
+    
+                AjouterAlimentBoissonScreen(menuScanner);
                 break;
             default:
-                showGestionPlatScreen(menuScanner);
+                showGestionBoissonScreen(menuScanner);
         }
     }
+    
 
+    public static void RetirerAlimentBoissonScreen(Scanner menuScanner) throws IOException {
+        clearConsole();
+        print("==========================================================================\n");
+        print("Menu de Gestion du Stock des aliments pour les boissons : \n");
+        print("--------------------------------------------------------------------------");
+        print("                  Retirer des aliments des boissons");
+        print("--------------------------------------------------------------------------\n");
+        print("0 - Retour au menu gestion de stock des aliments des boissons\n");
+        print("1 - Retirer des aliments au stock des aliments des boissons\n");
+
+        String choixEcran = menuScanner.next();
+        switch (choixEcran) {
+            case "0":
+                // Retour du screen
+                showGestionBoissonScreen(menuScanner);
+                break;
+            case "1":
+                // Utilisation de la fonction lireFichier de la classe Stock pour récupérer la liste d'aliments
+                List<Boisson> listeAliments = StockDrink.lireFichier("src\\main\\data\\stockDrink.txt");
+                print("\n--------------------------------------------------------------------------\n");
+                // Affichage des aliments
+                for (Boisson aliment : listeAliments) {
+                    System.out.println(aliment.getNom() + ": " + aliment.getQuantite());
+                }
+                print("\n--------------------------------------------------------------------------\n");
+                boolean alimentExiste = false;
+                String nomAliment;
+                do {
+                    // Nettoyer le tampon avant de lire le nom de l'aliment
+                    menuScanner.nextLine();
+
+                    // Demander à l'utilisateur le nom de l'aliment à retirer
+                    print("Entrez le nom de l'aliment à retirer : \n");
+                    nomAliment = menuScanner.nextLine();
+
+                    // Vérifier si l'aliment est dans la liste (en comparant le nom complet)
+                    for (Boisson aliment : listeAliments) {
+                        // Utilisez equalsIgnoreCase pour la comparaison insensible à la casse
+                        if (aliment.getNom().equalsIgnoreCase(nomAliment)) {
+                            alimentExiste = true;
+                            break;
+                        }
+                    }
+
+                    if (!alimentExiste) {
+                        print("Cet aliment n'est pas dans la liste. Veuillez réessayer.\n");
+                    }
+
+                } while (!alimentExiste);
+    
+                print("\nEntrez la quantité de l'aliment à retirer : \n");
+                int quantiteAliment;
+                do {
+                    // Assurez-vous que la quantité est un entier positif
+                    try {
+                        quantiteAliment = menuScanner.nextInt();
+
+                        // Vérifier si la quantité à retirer est supérieure à la quantité en stock
+                        int quantiteEnStock = getBoissonQuantiteEnStock(listeAliments, nomAliment);
+                        if (quantiteAliment > quantiteEnStock) {
+                            print("La quantité à retirer ne peut pas être supérieure à la quantité en stock. Veuillez réessayer.\n");
+                            continue;
+                        }
+
+                        if (quantiteAliment <= 0) {
+                            print("La quantité doit être positive. Veuillez réessayer.\n");
+                        }
+                    } catch (InputMismatchException e) {
+                        print("Veuillez entrer un nombre entier. Veuillez réessayer.\n");
+                        quantiteAliment = -1; // Définir une valeur par défaut pour éviter une boucle infinie
+                    }
+                } while (quantiteAliment <= 0);
+    
+                // Créer une instance de HashMap et retirer l'aliment à retirer
+                Map<String, Integer> alimentARetirerMap = new HashMap<>();
+                alimentARetirerMap.put(nomAliment, quantiteAliment);
+
+    
+                // Appel de la fonction pour retirer l'aliment du stock
+                StockDrink.retirerBoisson("src\\main\\data\\stockDrink.txt", alimentARetirerMap);
+    
+                RetirerAlimentBoissonScreen(menuScanner);
+                break;
+            default:
+                showGestionBoissonScreen(menuScanner);
+        }
+    }
+//--------------------------------------------------------------------------------------------------------
 
     public static void print(String text) {
         System.out.println(text);
@@ -278,5 +545,21 @@ public class GestionStockScreen {
     public static void clearConsole() {
         for (int i = 0; i < 50; ++i)
             System.out.println();
-    }   
+    }
+    private static int getBoissonQuantiteEnStock(List<Boisson> listeAliments, String nomAliment) {
+        for (Boisson aliment : listeAliments) {
+            if (aliment.getNom().equalsIgnoreCase(nomAliment)) {
+                return aliment.getQuantite();
+            }
+        }
+        return 0; // Si l'aliment n'est pas trouvé, la quantité en stock est considérée comme 0
+    }
+    private static int getQuantiteEnStock(List<Aliment> listeAliments, String nomAliment) {
+        for (Aliment aliment : listeAliments) {
+            if (aliment.getNom().equalsIgnoreCase(nomAliment)) {
+                return aliment.getQuantite();
+            }
+        }
+        return 0; // Si l'aliment n'est pas trouvé, la quantité en stock est considérée comme 0
+    }
 }
